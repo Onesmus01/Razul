ecde9efb2ef8f02071edbaea1d8879517f7251ff
Onesmus Wambua <onesmuswambua747@gmail.com>
2026-01-06 17:35:53 +0300

    backend api

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/api/user',userRouter)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
+
+app.get('/',(req,res)=> {
+    res.send('API is running...')
+})
 
 app.listen(PORT,()=> {console.log(`Server is running on port ${PORT}`)
 connectDB()
